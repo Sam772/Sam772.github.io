b9e865701ba333bad05f4a57db593029ffcecfd2
Sam772 <78389553+Sam772@users.noreply.github.com>
2022-01-14 13:34:56 +0000

    Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaAnkh, FaFire} from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <div className=''>
-            <i>Nav</i>
-            <i>Nav</i>
-            <i>Nav</i>
-            <i>Nav</i>
-            <i>Nav</i>
+        <div className="fixed top-0 left-0 h-screen w-16 flex 
+                        flex-col bg-gray-900 text-white shadow-lg">
+            <NavIcon icon={<FaFire size="28"/>}/>
+            <NavIcon icon={<BsPlus size="32"/>}/>
+            <NavIcon icon={<BsFillLightningFill size="20"/>}/>
+            <NavIcon icon={<BsGearFill size="28"/>}/>
+            <NavIcon icon={<FaAnkh size="20"/>}/>
         </div>
     )
 }
+
+const NavIcon = ({icon}) => {
+    return (
+        <div className="navbar-icon">
+            {icon}
+        </div>
+    )
+}
+
 
 export default Navbar
