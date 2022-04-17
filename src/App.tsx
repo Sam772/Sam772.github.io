c17ccfebb4fd93d1f84e493aaa193c1398b23ab4
Sam <sssam.8390@gmail.com>
2022-04-18 00:27:14 +0100

    Added responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,16 @@ function App() {
   return (
     <>
       <Router>
-      <div className="absolute h-full w-full bg-[url('/src/images/beams.png')] bg-cover bg-center bg-fixed no-scrollbar overflow-y-auto">
-        <div className="absolute h-full w-full bg-[url('/src/images/grid.svg')] [background-position:calc(100%+5px)_calc(100%+5px)] bg-fixed" />
-          <div className="absolute w-full bg-[url('/src/images/beams.png')] bg-cover bg-center bg-fixed">
-            <div className="absolute h-full w-full bg-[url('/src/images/grid.svg')] [background-position:calc(100%+5px)_calc(100%+5px)] bg-fixed" />
-              <Navbar />
-                <Routes>
-                  <Route path = "/" element = {<Home />} />
-                  <Route path = "/projects" element = {<Projects />} />
-                  <Route path = "/contact" element = {<Contact />} />
-                </Routes>
+        <div className="absolute h-full w-full bg-[url('/src/images/beams.png')] bg-cover bg-center bg-fixed no-scrollbar overflow-y-auto">
+          <div className="absolute h-full w-full bg-[url('/src/images/grid.svg')] [background-position:calc(100%+5px)_calc(100%+5px)] bg-fixed">
+            <Navbar />
+            <Routes>
+              <Route path = "/" element = {<Home />} />
+              <Route path = "/projects" element = {<Projects />} />
+              <Route path = "/contact" element = {<Contact />} />
+            </Routes>
           </div>
-      </div>
+        </div>
       </Router>
     </>
   );
