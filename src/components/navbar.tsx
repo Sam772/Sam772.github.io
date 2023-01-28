6adfc1c272539166c3e86cc6eead32341981457a
Sam <sssam.8390@gmail.com>
2023-01-28 16:00:04 +0000

    Fixed navbar button hitbox

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,25 +13,25 @@ function Navbar() {
               <div className="flex items-center">
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-6">
-                    <div className='group [transform:translateZ(0)] px-4 py-2 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-slate-500 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500'>
-                      <Link to ="/" className="relative z-0 text-black group-hover:text-gray-400 transition ease-in-out duration-500 text-lg">
-                        Home
+                      <Link to ="/" className="group [transform:translateZ(0)] px-4 py-2 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-slate-500 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500">
+                        <div className='relative z-0 text-black group-hover:text-gray-400 transition ease-in-out duration-500 text-lg'>
+                          Home
+                        </div>
                       </Link>
-                    </div>
-                    <div className='group [transform:translateZ(0)] px-4 py-2 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-slate-500 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500'>
-                      <Link to ="/projects" className="relative z-0 text-black group-hover:text-gray-400 transition ease-in-out duration-500 text-lg">
-                        Projects
+                      <Link to ="/projects" className="group [transform:translateZ(0)] px-4 py-2 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-slate-500 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500">
+                        <div className='relative z-0 text-black group-hover:text-gray-400 transition ease-in-out duration-500 text-lg'>
+                          Projects
+                        </div>
                       </Link>
-                    </div>
-                    <div className='group [transform:translateZ(0)] px-4 py-2 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-slate-500 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500'>
-                      <Link to ="/contact" className="relative z-0 text-black group-hover:text-gray-400 transition ease-in-out duration-500 text-lg">
-                        Contact
+                      <Link to ="/contact" className="group [transform:translateZ(0)] px-4 py-2 rounded-lg overflow-hidden bg-gray-200 relative before:absolute before:bg-slate-500 before:top-1/2 before:left-1/2 before:h-8 before:w-8 before:-translate-y-1/2 before:-translate-x-1/2 before:rounded-full before:scale-[0] before:opacity-0 hover:before:scale-[6] hover:before:opacity-100 before:transition before:ease-in-out before:duration-500">
+                        <div className='relative z-0 text-black group-hover:text-gray-400 transition ease-in-out duration-500 text-lg'>
+                          Contact
+                        </div>
                       </Link>
-                    </div>
                   </div>
                 </div>
               </div>
-              <div className="-mr-2 flex md:hidden">
+              <div className="mr-2 flex md:hidden">
                 <button
                   onClick={() => setIsOpen(!isOpen)}
                   type="button"
